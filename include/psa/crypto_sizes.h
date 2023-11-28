632a466ee86804e9dae1bd91a5c47d513a3b03be
Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
2023-11-28 12:53:30 +0100

    [nrf noup] Remove redefinition ECC_MAX_CURVE_BITS
    -This removes the redefinition of the define
     PSA_VENDOR_ECC_MAX_CURVE_BITS which we allow to
     be set in our configuration file from nrf security.
    
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>

--- include/psa/crypto_sizes.h
+++ include/psa/crypto_sizes.h
@@ -264,6 +264,7 @@
 
 /* The maximum size of an ECC key on this implementation, in bits.
  * This is a vendor-specific macro. */
+#ifndef PSA_VENDOR_ECC_MAX_CURVE_BITS
 #if defined(PSA_WANT_ECC_SECP_R1_521)           /*!!OM*/
 #define PSA_VENDOR_ECC_MAX_CURVE_BITS 521u
 #elif defined(PSA_WANT_ECC_BRAINPOOL_P_R1_512)
@@ -296,6 +297,7 @@
 #define PSA_VENDOR_ECC_MAX_CURVE_BITS 192u
 #else
 #define PSA_VENDOR_ECC_MAX_CURVE_BITS 0u
+#endif
 #endif
 
 /** This macro returns the maximum supported length of the PSK for the
